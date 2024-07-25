801d38f92a0e95eb24bb5b3a11765c77f2ec943e
thiagoolisilva <thiago_tos12@hotmail.com>
2024-07-25 14:08:57 -0300

    Implementação do Footer da Pagina

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,42 +1,59 @@
 <script setup></script>
 
 <template>
-    <!-- Footer Start -->
     <footer class="bg-theme-dark-blue py-10">
         <div class="container mx-auto px-5">
-            <div class="grid md:grid-cols-2">
-                <div class="flex flex-col md:flex-row md:space-x-12 items-center">
-                    <div class="mb-10 md:mb-0">
-                        <img src="/images/logo-bookmark-white.svg" alt="Logo" />
-                    </div>
-                    <ul class="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-8 text-xs text-center md:text-left font-theme-heading">
-                        <li class="uppercase text-white hover:text-theme-secondary transition duration-200">
-                            <a href="#features" v-smooth-scroll>Features</a>
-                        </li>
-                        <li class="uppercase text-white hover:text-theme-secondary transition duration-200">
-                            <a href="#faq" v-smooth-scroll>FAQ</a>
-                        </li>
-                        <li class="uppercase text-white hover:text-theme-secondary transition duration-200">
-                            <a href="#download-section" v-smooth-scroll>Download</a>
-                        </li>
+            <!-- Grid com Principais Informações -->
+            <div class="grid md:grid-cols-4 gap-8">
+                <div class="flex justify-center md:justify-start">
+                    <!-- Imagem do Header -->
+                    <img src="/images/hinova-mobile-branco.png" class="w-17 h-12" alt="Logo" />
+                </div>
+                <div>
+                    <h4 class="text-white font-bold mb-4">Produtos</h4>
+                    <ul class="space-y-2">
+                        <li class="text-white">Associado</li>
+                        <li class="text-white">Consultor</li>
+                        <li class="text-white">Visto</li>
                     </ul>
                 </div>
-                <div class="md:place-self-end place-self-center mt-10 md:mt-0">
+                <div>
+                    <h4 class="text-white font-bold mb-4">Parceiros</h4>
+                    <ul class="space-y-2">
+                        <li class="text-white">Grupo Hinova</li>
+                        <li class="text-white">Power CRM</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4 class="text-white font-bold mb-4">Contato</h4>
+                    <ul class="space-y-2">
+                        <li class="text-white"><strong>Telefone:</strong> (XX) XXXX-XXXX</li>
+                        <li class="text-white"><strong>Endereço:</strong> Rua Manoel Elias de Aguiar, 245 – Ouro Preto BH/MG</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="flex justify-center mt-8">
                     <ul class="flex space-x-8 items-center">
                         <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-facebook text-white hover:text-theme-secondary transition duration-200 cursor-pointer" viewBox="0 0 16 16">
-                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                            </svg>
+                            <!-- Link para o Facebook Hinova -->
+                            <a href="https://www.facebook.com/profile.php?id=100095028497790" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-facebook text-white hover:text-theme-secondary transition duration-200 cursor-pointer" viewBox="0 0 16 16">
+                                    <!-- Este path é o codigo SVG para o Icone do Facebook -->
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                </svg>
+                            </a>
                         </li>
                         <li>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-twitter text-white hover:text-theme-secondary transition duration-200 cursor-pointer" viewBox="0 0 16 16">
-                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                            </svg>
+                            <!-- Link para o Instagram Hinova -->
+                            <a href="https://www.instagram.com/hinovasolucoesdigitais/?hl=pt-br" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-instagram text-white hover:text-theme-secondary transition duration-200 cursor-pointer" viewBox="0 0 16 16">
+                                    <!-- Este path é o codigo SVG para o Icone do Instagram -->
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                                </svg>
+                            </a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </div>
     </footer>
-    <!-- Footer End -->
 </template>
